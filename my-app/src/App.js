768cede64d2e7fe63ecbc266503acb3e114578ee
JoeYip13 <>
2023-09-19 09:42:39 +0000

    feat(components): handling events in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
       <StatefulGreeting greeting="I am a stateful class component!" name="Joe" />
+      <EventsClass />
+      <EventsFunctional />
     </div>
   );
 }
