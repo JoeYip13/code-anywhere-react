09d887eccffe5ce5c728cf47c4cc071504353c7d
JoeYip13 <>
2023-09-24 17:12:00 +0000

    feat(components): rendering the data & handle error

--- my-app/src/components/HTTPRequest.js
+++ my-app/src/components/HTTPRequest.js
@@ -6,7 +6,8 @@ export class HTTPRequest extends Component {
         super(props);
 
         this.state = {
-            posts: []
+            posts: [],
+            error: null
         };
     }
     componentDidMount() {
@@ -15,15 +16,45 @@ export class HTTPRequest extends Component {
         .then(response => {
             console.log(response)
             this.setState({
-                posts: response.data
+                // javascript built in method isArray() to check if the response data is already a array
+                posts: Array.isArray(response.data)
+                // sets post to response.data
+                ? response.data
+                // otherwise manually put response.data into a array
+                : [response.data]
+            })
+        })
+        // catch errors
+        .catch(error => {
+            this.setState({
+                error: error.message
             })
         })
     }
     render() {
+        const posts = this.state.posts;
         return (
             <div>
                 <h2>Posts</h2>
-                {JSON.stringify(this.state.posts)}
+                {
+                    posts.length ? (
+                        posts.map(post => (
+                            <div key={post.id}>
+                                <h2>{post.id}. {post.title}</h2>
+                                <h4>By user id:{post.userid}</h4>
+                                <p>{post.body}</p>
+                                <hr/>
+                            </div>
+                        ))
+                    ) : (
+                        // check for error
+                        this.state.error
+                        // if error display the error
+                        ? <p>{this.state.error}</p>
+                        // otherwise 
+                        : <h4>Loading Posts...</h4>
+                    )
+                }
             </div>
         );
     }
