c721f727583713a574d131287421f784c76af769
JoeYip13 <>
2023-09-24 12:12:34 +0000

    feat(components): useEffect() conditional effects

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -5,14 +5,28 @@ function UseEffectCounter() {
     // useEffect replaces the componentDidMount and componentDidUpdate in class based components
     // useEffect runs the function you give it after evey render
     useEffect(() => {
+        console.log('Count 1 effect');
         document.title = count;
-    });
+    }, [count]);
+    // when using useEffect hook, you should nearly always specify the secound parameter. I.e [count2]..
+    // which is the state property that will only run the useEffect when the state changes
+    // if you remove the second parameter, calling the increment count 1 button will also call the useEffect for count2
+    const [count2, setCount2] = useState(0);
+    useEffect(() => {
+        console.log('Count 2 effect');
+        document.title = count;
+    }, [count2]);
     return (
         <div>
             <button onClick={
                 () => setCount(count => count + 1)
             }>
-                Increment Count ({count})
+                Increment Count 1 ({count})
+            </button>
+            <button onClick={
+                () => setCount2(count2 => count2 + 1)
+            }>
+                Increment Count 2 ({count2})
             </button>
         </div>
     )
